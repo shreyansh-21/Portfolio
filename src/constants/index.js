27487a8b7cf46ae2b88e45521e1271feb97df18d
Shreyansh Dubey <shreyansh2128@gmail.com>
2025-06-28 00:55:23 +0530

    fix: update resume link in publicUrls for accessibility

--- src/constants/index.js
+++ src/constants/index.js
@@ -314,7 +314,7 @@ const personalInfo = {
 
 const publicUrls = {
   resume:
-    "https://drive.google.com/file/d/1Af69XJ2ar4SG7oWrkyzZzJJE8QxNOyPb/view?usp=sharing",
+    "https://drive.google.com/file/d/1nozEWMt5XaYGhPdmggglbyaq1LmQower/view?usp=sharing",
   socialProfiles: {
     linkedin: {
       title: "linkedin",
